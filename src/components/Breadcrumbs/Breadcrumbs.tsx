628f34f1dac516ec23c80cddd3b72a39b41fd203
Dariia Romanova <dariia.romanova@gmail.com>
2022-01-03 13:16:35 +0200

    Fix

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -7,7 +7,7 @@ export const Breadcrumbs: React.FC<Props> = ({ page }) => {
     <div className="breadcrumbs">
       <ul className="breadcrumbs__list">
         <li className="breadcrumbs__item">
-          <a href="/" className="breadcrumbs__link">Home</a>
+          <a href="/plantin-blog" className="breadcrumbs__link">Home</a>
         </li>
         <li className="breadcrumbs__item">
           {page}
