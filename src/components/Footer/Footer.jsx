89508b976c06b3d231968e1804ebf1ccf314ba9c
Dariia Romanova <dariia.romanova@gmail.com>
2022-01-04 13:48:41 +0200

    Href fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,21 @@ export const Footer = () => {
       <div className="footer__content">
         <nav>
           <ul className="footer__list">
-            <li className="footer__item">Terms of Service</li>
-            <li className="footer__item">Privacy Policy</li>
-            <li className="footer__item">Subscription Policy</li>
+            <li className="footer__item">
+              <a className="footer__link" href="/plantin-blog">
+                Terms of Service
+              </a>
+            </li>
+            <li className="footer__item">
+              <a className="footer__link" href="/plantin-blog">
+                Privacy Policy
+              </a>
+            </li>
+            <li className="footer__item" href="/plantin-blog">
+              <a className="footer__link" href="/plantin-blog">
+                Subscription Policy
+              </a>
+            </li>
           </ul>
         </nav>
         <p>©Copyright  © 2020 PlantIn. All rights reserved</p>
